9a6078c086b0b5e076adee5a90d2c8f8163a6823
dario-nihil <dario.nihil@gmail.com>
2023-01-14 19:47:14 +0100

    implemented context to manage global state for latLng and coffeShops

--- pages/_app.js
+++ pages/_app.js
@@ -1,9 +1,26 @@
+import { createContext } from "react";
+
 import "../styles/globals.css";
+
+const StoreContext = createContext();
+
+const StoreProvider = ({ children }) => {
+  const initialState = {
+    latLng: "",
+    coffeeStores: [],
+  };
+
+  return (
+    <StoreContext.Provider value={{ state: initialState }}>
+      {children}
+    </StoreContext.Provider>
+  );
+};
 
 export default function App({ Component, pageProps }) {
   return (
-    <>
+    <StoreProvider>
       <Component {...pageProps} />
-    </>
+    </StoreProvider>
   );
 }
