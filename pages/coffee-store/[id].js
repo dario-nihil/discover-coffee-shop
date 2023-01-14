484b3780ff6f967c1cb57c94329d4977293113eb
dario-nihil <dario.nihil@gmail.com>
2023-01-14 15:49:28 +0100

    fix styles bug in home page

--- pages/coffee-store/[id].js
+++ pages/coffee-store/[id].js
@@ -28,7 +28,7 @@ const CoffeeStore = ({
       <div className={styles.container}>
         <div className={styles.col1}>
           <div className={styles.backToHomeLink}>
-            <Link href="/">Back to home</Link>
+            <Link href="/">←Back to home</Link>
           </div>
           <div className={styles.nameWrapper}>
             <h1 className={styles.name}>{name}</h1>
