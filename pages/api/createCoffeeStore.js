9e11d25158cbcd58d95938dcfa7f91be4eb48723
dario-nihil <dario.nihil@gmail.com>
2023-01-16 19:47:52 +0100

    implemeted better error handling and refactored code

--- pages/api/createCoffeeStore.js
+++ pages/api/createCoffeeStore.js
@@ -1,46 +1,48 @@
-const Airtable = require("airtable");
-
-const base = new Airtable({ apiKey: process.env.AIRTABLE_API_KEY }).base(
-  process.env.AIRTABLE_BASE_KEY
-);
-
-const table = base("coffee-stores");
+import { table, getMinifiedRecords } from "../../lib/airtable";
 
 export const handler = async (req, res) => {
   if (req.method === "POST") {
     const { id, name, neighbourhood, address, imgUrl, voting } = req.body;
 
     try {
-      const findCoffeeStoreRecords = await table
-        .select({
-          filterByFormula: `id = '${id}'`,
-        })
-        .firstPage();
+      if (id) {
+        const findCoffeeStoreRecords = await table
+          .select({
+            filterByFormula: `id = '${id}'`,
+          })
+          .firstPage();
 
-      if (findCoffeeStoreRecords.length !== 0) {
-        const records = findCoffeeStoreRecords.map((record) => ({
-          ...record.fields,
-        }));
-        res.json({ message: "Return existing record", records });
+        if (findCoffeeStoreRecords.length !== 0) {
+          const records = getMinifiedRecords(findCoffeeStoreRecords);
+          res.json({ message: "Return existing record", records });
+        } else {
+          if (name) {
+            const createRecord = await table.create([
+              {
+                fields: {
+                  id,
+                  name,
+                  address,
+                  neighbourhood,
+                  voting,
+                  imgUrl,
+                },
+              },
+            ]);
+            const records = getMinifiedRecords(createRecord);
+            res.json({ message: "create a record", records });
+          } else {
+            res.status(400).json({ message: "Id or name is missing!" });
+          }
+        }
       } else {
-        const createRecord = await table.create([
-          {
-            fields: {
-              id,
-              name,
-              address,
-              neighbourhood,
-              voting,
-              imgUrl,
-            },
-          },
-        ]);
-        const records = createRecord.map((record) => ({ ...record.fields }));
-        res.json({ message: "create a record", records });
+        res.status(400).json({ message: "Id is missing!" });
       }
     } catch (error) {
-      console.log("Error finding store", error);
-      res.status(500).json({ message: "Error finding store", error });
+      console.log("Error creating or finding store", error);
+      res
+        .status(500)
+        .json({ message: "Error creating or finding store", error });
     }
   }
 };
