7cbb5b9f540bb8098a98a045324f3e54e8e16052
dario-nihil <dario.nihil@gmail.com>
2023-01-16 18:55:55 +0100

    implemented getting record by id from airtable

--- pages/api/createCoffeeStore.js
+++ pages/api/createCoffeeStore.js
@@ -1,9 +1,34 @@
 const Airtable = require("airtable");
-Airtable.configure({
-  endpointUrl: "https://api.airtable.com",
-  apiKey: process.env.AIRTABLE_API_KEY,
-});
 
-const base = Airtable.base(process.env.AIRTABLE_BASE_KEY);
+const base = new Airtable({ apiKey: process.env.AIRTABLE_API_KEY }).base(
+  process.env.AIRTABLE_BASE_KEY
+);
 
 const table = base("coffee-stores");
+
+export const handler = async (req, res) => {
+  if (req.method === "POST") {
+    try {
+      const findCoffeeStoreRecords = await table
+        .select({
+          filterByFormula: "id = '0'",
+        })
+        .firstPage();
+
+      if (findCoffeeStoreRecords.length !== 0) {
+        const records = findCoffeeStoreRecords.map((record) => ({
+          ...record.fields,
+        }));
+        console.log(records);
+        res.json({ records });
+      } else {
+        res.json({ message: "create a record" });
+      }
+    } catch (error) {
+      console.log("Error finding store", error);
+      res.status(500).json({ message: "Error finding store", error });
+    }
+  }
+};
+
+export default handler;
