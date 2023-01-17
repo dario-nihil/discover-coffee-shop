cbaa408952ef5c905ccf58455383f773c8f8f885
dario-nihil <dario.nihil@gmail.com>
2023-01-17 10:50:48 +0100

    implemented get functionality

--- pages/api/getCoffeeStoreById.js
+++ pages/api/getCoffeeStoreById.js
@@ -1,9 +1,22 @@
-const handler = (req, res) => {
+import { table, getMinifiedRecords } from "../../lib/airtable";
+
+const handler = async (req, res) => {
   const { id } = req.query;
 
   try {
     if (id) {
-      res.status(201).json({ message: `id is created ${id}` });
+      const findCoffeeStoreRecords = await table
+        .select({
+          filterByFormula: `id = '${id}'`,
+        })
+        .firstPage();
+
+      if (findCoffeeStoreRecords.length !== 0) {
+        const records = getMinifiedRecords(findCoffeeStoreRecords);
+        res.json({ message: "Return existing record", records });
+      } else {
+        res.status(404).json({ message: `id could not be found - ${id}` });
+      }
     } else {
       res.status(500).json({ message: "Id is missing" });
     }
